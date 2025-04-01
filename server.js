31d4cbff6a1983066ec7d7c188801a9b9dea315d
JuHyeongLee <wngud1023@naver.com>
2025-04-01 23:55:27 +0900

    no message

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express();
 const test = require('./Router/test');
 
-app.use('/api', test);
+const cors = require('cors');
+
+// app.use('/api', test);
+app.use(cors());
+
 
 const port = 3002;
 app.listen(port, () => {
@@ -12,4 +16,9 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
     res.send('백엔드 루트 페이지입니다!')
+
+});
+
+app.get('/api', (req, res) => {
+    res.json({ message : 'React와 Express가 연결되었습니다!!!!' })
 });
