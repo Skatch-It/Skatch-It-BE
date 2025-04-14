13e71ea073bf8d49f0cdb8d2a8d4fde6b40a5e33
JuHyeong2 <100174932+JuHyeong2@users.noreply.github.com>
2025-04-14 20:17:14 +0900

    Sibe1 redis 데이터베이스 (#4)
    * redis 데이터베이스 적용 및 닉네임 중복 api 구현
    04-10
    
    * Delete node_modules directory
    
    * no message

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
+
 //server.js
+const Redis = require('ioredis')
+
+const redis = new Redis({
+    host : '127.0.0.1',
+    port: 6379
+})
 const express = require('express');
 const app = express();
 const test = require('./Router/test');
@@ -7,6 +14,7 @@ const cors = require('cors');
 
 // app.use('/api', test);
 app.use(cors());
+app.use(express.json())
 
 
 const port = 3002;
@@ -19,6 +27,25 @@ app.get('/', (req, res) => {
 
 });
 
-app.get('/api', (req, res) => {
+app.get('/get', (req, res) => {
     res.json({ message : 'React와 Express가 연결되었습니다!!!!' })
 });
+
+// 닉네임 중복 확인 API + redis데이터베이스에 저장
+app.post('/api/nickname-check', async(req, res) => {
+    const { nickname } = req.body
+    
+    if(!nickname || typeof nickname !== 'string'){
+        return res.status(400).json({ success: false, error: 'Invalid nickname' })
+    }
+
+    const isTaken = await redis.sismember('active_nicknames', nickname)
+
+    if(isTaken) {
+        return res.json({ success: true, duplicate: true })
+    }
+
+    // 중복이 아니면 추가
+    await redis.sadd('active_nicknames', nickname)
+    return res.json({ success: true, duplicate: false })
+})
